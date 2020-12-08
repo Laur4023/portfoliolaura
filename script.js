30db43c82d7a046f57920a4b161fa2d3997e01cf
Laur4023 <74530743+Laur4023@users.noreply.github.com>
2020-12-08 18:28:35 +0100

    ændringer i script

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ const navslide = ()=>{
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('links');
     const links = document.querySelectorAll('.links');
-    //toggle nav
+    
     burger.addEventListener('click',()=>{
+    //toggle nav
     nav.classList.toggle('nav-active')
-    });
+
     //animation af links
     links.forEach(link=>{
-    link.style.animation = `linkfade 0.5s ease forwards ${index / 7}s`
-    console.log(index / 7);
+    if(link.style.animation) {
+    link.style.animation = ''
+    } else {
+    link.style.animation = `linkfade 0.5s ease forwards ${index / 7 + 1.5}s`;
+        }
+    }); 
     });
+
 }
 navslide();
