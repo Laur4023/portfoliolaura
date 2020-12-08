74d209f98ad063a9444dc18a4959a60df661a211
Laur4023 <74530743+Laur4023@users.noreply.github.com>
2020-12-08 18:15:06 +0100

    Ny navbar
    udskiftet min navbar da den tidligere ikke virkede.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const menubutton = document.getElementsByClassName('menubutton')[0]
-const links = document.getElementsByClassName('links')[0]
-
-menubutton.addEventListener('click', () => {
-links.classList.toggle('active')
-})
+const navslide = ()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('links');
+    const links = document.querySelectorAll('.links');
+    //toggle nav
+    burger.addEventListener('click',()=>{
+    nav.classList.toggle('nav-active')
+    });
+    //animation af links
+    links.forEach(link=>{
+    link.style.animation = `linkfade 0.5s ease forwards ${index / 7}s`
+    console.log(index / 7);
+    });
+}
+navslide();
